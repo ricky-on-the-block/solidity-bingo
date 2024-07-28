b76ad998426f010be1ffe728e77f8a273692fe1d
Ric Waltman <ric.waltman@protonmail.com>
2024-07-28 13:08:36 -0700

    gameCost: passing test

--- src/Bingo.sol
+++ src/Bingo.sol
@@ -4,5 +4,11 @@ pragma solidity 0.8.26;
 import {IBingo} from "./IBingo.sol";
 
 contract Bingo {
+    uint256 public constant GAME_COST = 100 gwei;
+
     constructor() {}
+
+    function gameCost() public pure returns(uint256 gameCostWei) {
+        gameCostWei = GAME_COST;
+    }
 }
